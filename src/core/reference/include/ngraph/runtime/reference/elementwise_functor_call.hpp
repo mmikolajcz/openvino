f2f18c9e17f55af6b64f28504c4d2767efb36020
Mateusz <mateusz.mikolajczyk@intel.com>
2022-10-18 10:43:51 +0000

    Fix runtime function

--- src/core/reference/include/ngraph/runtime/reference/elementwise_functor_call.hpp
+++ src/core/reference/include/ngraph/runtime/reference/elementwise_functor_call.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <cstddef>
+#include <functional>
 
 namespace ngraph {
 namespace runtime {
@@ -12,7 +13,7 @@ namespace reference {
 template <typename T, typename U, typename Functor>
 void elementwise_functor_call(const T* input, U* output, size_t count, Functor functor) {
     for (size_t i = 0; i < count; i++) {
-        output[i] = functor(input[i]);
+        output[i] = static_cast<U>(functor(input[i]));
     }
 }
 }  // namespace reference
--- src/core/reference/include/ngraph/runtime/reference/is_inf.hpp
+++ src/core/reference/include/ngraph/runtime/reference/is_inf.hpp
@@ -4,25 +4,28 @@
 
 #pragma once
 
-#include <cstddef>
 #include <cmath>
+#include <cstddef>
+
 #include "ngraph/runtime/reference/elementwise_functor_call.hpp"
 
 namespace ngraph {
 namespace runtime {
 namespace reference {
 template <typename T, typename U>
-void is_inf(const T* input, U* output, size_t count, const ov::op::v10::IsInf::Attributes attributes) {
+void is_inf(const T* input, U* output, size_t count, const ov::op::v10::IsInf::Attributes& attributes) {
     if (attributes.detect_negative && attributes.detect_positive) {
-        runtime::reference::elementwise_functor_call(input, output, count, std::isinf);
+        runtime::reference::elementwise_functor_call(input, output, count, [](T x) -> U {
+            return (static_cast<U>(std::isinf(x)));
+        });
     } else if (!attributes.detect_negative && attributes.detect_positive) {
-        for (size_t i = 0; i < count; i++) {
-            output[i] = (input[i] == std::numeric_limits<T>::infinity());
-        }
+        runtime::reference::elementwise_functor_call(input, output, count, [](T x) -> U {
+            return (static_cast<U>(x == std::numeric_limits<T>::infinity()));
+        });
     } else if (attributes.detect_negative && !attributes.detect_positive) {
-        for (size_t i = 0; i < count; i++) {
-            output[i] = (input[i] == -std::numeric_limits<T>::infinity());
-        }
+        runtime::reference::elementwise_functor_call(input, output, count, [](T x) -> U {
+            return (static_cast<U>(x == -std::numeric_limits<T>::infinity()));
+        });
     } else {
         std::memset(output, 0, count);
     }
